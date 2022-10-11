8885fe4f40188297f5bbedbb9c84f83d0dddcb1f
Brian James <brianpja@gmail.com>
2022-10-11 19:13:31 -0400

    updated read me

---
+++ src/data/deck.tsx
@@ -0,0 +1,262 @@
+export const deck = [
+  {
+    number: 'ace',
+    suit: 'clubs',
+    value: 14
+  },
+  {
+    number: 'two',
+    suit: 'clubs',
+    value: 2
+  },
+  {
+    number: 'three',
+    suit: 'clubs',
+    value: 3
+  },
+  {
+    number: 'four',
+    suit: 'clubs',
+    value: 4
+  },
+  {
+    number: 'five',
+    suit: 'clubs',
+    value: 5
+  },
+  {
+    number: 'six',
+    suit: 'clubs',
+    value: 6
+  },
+  {
+    number: 'seven',
+    suit: 'clubs',
+    value: 7
+  },
+  {
+    number: 'eight',
+    suit: 'clubs',
+    value: 8
+  },
+  {
+    number: 'nine',
+    suit: 'clubs',
+    value: 9
+  },
+  {
+    number: 'ten',
+    suit: 'clubs',
+    value: 10
+  },
+  {
+    number: 'jack',
+    suit: 'clubs',
+    value: 11
+  },
+  {
+    number: 'queen',
+    suit: 'clubs',
+    value: 12
+  },
+  {
+    number: 'king',
+    suit: 'clubs',
+    value: 13
+  },
+  {
+    number: 'ace',
+    suit: 'diamonds',
+    value: 14
+  },
+  {
+    number: 'two',
+    suit: 'diamonds',
+    value: 2
+  },
+  {
+    number: 'three',
+    suit: 'diamonds',
+    value: 3
+  },
+  {
+    number: 'four',
+    suit: 'diamonds',
+    value: 4
+  },
+  {
+    number: 'five',
+    suit: 'diamonds',
+    value: 5
+  },
+  {
+    number: 'six',
+    suit: 'diamonds',
+    value: 6
+  },
+  {
+    number: 'seven',
+    suit: 'diamonds',
+    value: 7
+  },
+  {
+    number: 'eight',
+    suit: 'diamonds',
+    value: 8
+  },
+  {
+    number: 'nine',
+    suit: 'diamonds',
+    value: 9
+  },
+  {
+    number: 'ten',
+    suit: 'diamonds',
+    value: 10
+  },
+  {
+    number: 'jack',
+    suit: 'diamonds',
+    value: 11
+  },
+  {
+    number: 'queen',
+    suit: 'diamonds',
+    value: 12
+  },
+  {
+    number: 'king',
+    suit: 'diamonds',
+    value: 13
+  },
+  {
+    number: 'ace',
+    suit: 'clubs',
+    value: 14
+  },
+  {
+    number: 'two',
+    suit: 'spades',
+    value: 2
+  },
+  {
+    number: 'three',
+    suit: 'spades',
+    value: 3
+  },
+  {
+    number: 'four',
+    suit: 'spades',
+    value: 4
+  },
+  {
+    number: 'five',
+    suit: 'spades',
+    value: 5
+  },
+  {
+    number: 'six',
+    suit: 'spades',
+    value: 6
+  },
+  {
+    number: 'seven',
+    suit: 'spades',
+    value: 7
+  },
+  {
+    number: 'eight',
+    suit: 'spades',
+    value: 8
+  },
+  {
+    number: 'nine',
+    suit: 'spades',
+    value: 9
+  },
+  {
+    number: 'ten',
+    suit: 'spades',
+    value: 10
+  },
+  {
+    number: 'jack',
+    suit: 'spades',
+    value: 11
+  },
+  {
+    number: 'queen',
+    suit: 'spades',
+    value: 12
+  },
+  {
+    number: 'king',
+    suit: 'spades',
+    value: 13
+  },
+  {
+    number: 'ace',
+    suit: 'hearts',
+    value: 14
+  },
+  {
+    number: 'two',
+    suit: 'hearts',
+    value: 2
+  },
+  {
+    number: 'three',
+    suit: 'hearts',
+    value: 3
+  },
+  {
+    number: 'four',
+    suit: 'hearts',
+    value: 4
+  },
+  {
+    number: 'five',
+    suit: 'hearts',
+    value: 5
+  },
+  {
+    number: 'six',
+    suit: 'hearts',
+    value: 6
+  },
+  {
+    number: 'seven',
+    suit: 'hearts',
+    value: 7
+  },
+  {
+    number: 'eight',
+    suit: 'hearts',
+    value: 8
+  },
+  {
+    number: 'nine',
+    suit: 'hearts',
+    value: 9
+  },
+  {
+    number: 'ten',
+    suit: 'hearts',
+    value: 10
+  },
+  {
+    number: 'jack',
+    suit: 'hearts',
+    value: 11
+  },
+  {
+    number: 'queen',
+    suit: 'hearts',
+    value: 12
+  },
+  {
+    number: 'king',
+    suit: 'hearts',
+    value: 13
+  },
+]
