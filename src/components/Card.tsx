dcf12522545a25316692006da2213ee3a046af96
Brian James <brianpja@gmail.com>
2022-10-11 19:46:55 -0400

    added icons

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,2 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
+const Card = ({}) => {
+  return (
+    <div className="card">
+      <span>K <FontAwesomeIcon icon={solid('heart')} /></span>
+    </div>
+  )
+};
+
+export default Card;
